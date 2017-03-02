78520758f71887d7dd6fd72d6c2fdacca3ee5e9e
Jan van Mansum <jan.van.mansum@dans.knaw.nl>
2017-03-02 15:03:18 +0100

    Updated

--- templates/fact.py
+++ templates/fact.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
-import json
 import functions
+import json
 
 print json.dumps({
     "version": functions.version_from_textfile("{{ easy_modules_installation_base_path }}/{{ easy_module_name }}/version")
